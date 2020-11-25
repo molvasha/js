0d8233268ab748762f72b983dbc9510a12d63fcd
Valeriya <molvasha@gmail.com>
2020-11-25 18:58:00 +0300

    last exercise

--- HW1/fifth.js
+++ HW1/fifth.js
@@ -1,3 +1,20 @@
 function mathOperation(a, b, operation) {
-  switch (a) {
-    case 3:
+  switch (operation) {
+    case "+":
+      alert(add(a, b));
+      break;
+    case "-":
+      alert(minus(a, b));
+      break;
+    case "*":
+      alert(multiply(a, b));
+      break;
+    case "/":
+      alert(section(a, b));
+      break;
+  }
+}
+
+var op = prompt('Введите операцию')
+
+mathOperation(a, b, op)
